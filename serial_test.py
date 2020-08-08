b26677364fbed6fac469c269e05a7bb7a587f262
Nicholas Robinson <nicholasrobinson@NicholasMacBook.localdomain>
2020-08-08 16:05:23 -0700

    Exposing axis control

--- serial_test.py
+++ serial_test.py
@@ -1,37 +1,51 @@
 import serial
 import time
+
 # Constants
 ENDIAN = 'big'
-CENTER_STICKS = '02 05 dc 05 dc 05 dc 05 dc 00 00 00 00 86'
+RX_BYTE_LENGTH = 100
+AXIS_MIN = 1000
+AXIS_MAX = 2000
+
+def sanitize_input(axis):
+    axis_output = axis * (AXIS_MAX - AXIS_MIN) + AXIS_MIN
+    if axis_output > AXIS_MAX:
+        return AXIS_MAX
+    elif axis_output < AXIS_MIN:
+        return AXIS_MIN
+    else:
+        return int(axis_output)
 
 # Convert data
-def generate_payload(axis1=1500, axis2=1500, axis3=1500, axis4=1500):
-    mode        = b'\x02'
-    axis1       += 100
-    axis1_bytes = axis1.to_bytes(2, byteorder=ENDIAN)
-    axis2_bytes = axis2.to_bytes(2, byteorder=ENDIAN)
-    axis3_bytes = axis3.to_bytes(2, byteorder=ENDIAN)
-    axis4_bytes = axis4.to_bytes(2, byteorder=ENDIAN)
-    fill        = b'\x00\x00\x00\x00'
-    partial_payload = mode + axis1_bytes + axis2_bytes + axis3_bytes + axis4_bytes + fill
-    check   = (sum(partial_payload) & 0xff).to_bytes(1, byteorder=ENDIAN)
-    return partial_payload + check
+def generate_payload(axis1=0.5, axis2=0.5, axis3=0.5, axis4=0.5):
+    axis1_output    = sanitize_input(axis1)
+    axis2_output    = sanitize_input(axis2)
+    axis3_output    = sanitize_input(axis3)
+    axis4_output    = sanitize_input(axis4)
+    mode            = b'\x02'
+    axis1_bytes     = axis1_output.to_bytes(2, byteorder=ENDIAN)
+    axis2_bytes     = axis2_output.to_bytes(2, byteorder=ENDIAN)
+    axis3_bytes     = axis3_output.to_bytes(2, byteorder=ENDIAN)
+    axis4_bytes     = axis4_output.to_bytes(2, byteorder=ENDIAN)
+    fill_bytes      = b'\x00\x00\x00\x00'
+    partial_payload = mode + axis1_bytes + axis2_bytes + axis3_bytes + axis4_bytes + fill_bytes
+    check_bytes     = (sum(partial_payload) & 0xff).to_bytes(1, byteorder=ENDIAN)
+    return partial_payload + check_bytes
 
-raw_data_strings = [
-      CENTER_STICKS,
-      generate_payload().hex(), # Experiment
-      CENTER_STICKS,
-]
-data_strings = [raw_data_string.replace(' ','') for raw_data_string in raw_data_strings]
-datas = [bytes.fromhex(data_string) for data_string in data_strings]
 # Write data
-ser = serial.Serial('/dev/cu.usbserial-DN2VI335', baudrate=57600, timeout=1)
-# ser = serial.Serial('/tmp/serial')
-for data in datas:
-    print('writing', data)
-    # byte_array = bytearray(data)
-    # checksum = hex(sum(byte_array[:-1]) & 0xff)
-    # print('checksum', checksum)
+ser = serial.Serial('/dev/cu.usbserial-DN2VI335', baudrate=57600, timeout=0.1)
+axis1 = 0.0
+axis2 = 0.0
+axis3 = 0.0
+axis4 = 0.0
+for i in range(100):
+    data = generate_payload(axis1, axis2, axis3, axis4)
+    print('writing', axis1, data)
     ser.write(data)
-    print('reading', ser.read(1000))
+    print('reading', ser.read(RX_BYTE_LENGTH))
+    axis1 += 0.01
+    axis2 += 0.01
+    axis3 += 0.01
+    axis4 += 0.01
+
 ser.close()
